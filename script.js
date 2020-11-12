d0106809906c5041035e057511b6e91984dd6a23
TOMIOKA Hiroshi <tomioka.hiroshi@gmail.com>
2020-11-12 17:59:49 +0900

    20201112

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var imgChange = function(param) {
-    var source_src = param.src;
-    var source_alt = param.alt;
-    var target = document.getElementById("gallery_main");
+const imgChange = (param) => {
+    const source_src = param.src;
+    const source_alt = param.alt;
+    const target = document.getElementById("gallery_main");
     target.setAttribute("src",source_src);
     target.setAttribute("alt",source_alt);
 }
